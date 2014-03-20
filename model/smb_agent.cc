722900af792f232b5e61293c38e65ea206011e57
Seth N. Hetu <seth.hetu@gmail.com>
2014-03-20 17:54:06 -0400

    Cleanup of the Agent and WFD_Agent classes.

--- model/smb_agent.cc
+++ model/smb_agent.cc
@@ -11,18 +11,23 @@
 #include "ns3/wifi-module.h"
 #include "ns3/olsr-helper.h"
 
+using namespace ns3;
+using namespace sm4ns3;
+
 namespace sm4ns3 {
-
-BaseWifi temp;
 //todo: for now and for the reason I dont know yet, I need to initialize like this otherwise m_wifi will not be initialized properly
+BaseWifi temp;
 BaseWifi Agent::m_wifi = temp;
-BaseMobility Agent::m_mobility;
-BaseIP Agent::m_ip;
-std::map<unsigned int, ns3::Ptr<Agent> > Agent::AllAgents;
-//sm4ns3::BaseFactory<Agent*> Agent::m_agentFactory;
+} //End namespae sm4ns3
 
-ns3::Ipv4Address
-GetPacketSource(Ptr<Packet> packet)
+
+sm4ns3::BaseMobility sm4ns3::Agent::m_mobility;
+sm4ns3::BaseIP sm4ns3::Agent::m_ip;
+std::map<unsigned int, ns3::Ptr<sm4ns3::Agent> > sm4ns3::Agent::AllAgents;
+
+
+namespace {
+ns3::Ipv4Address GetPacketSource(Ptr<Packet> packet)
 {
 	ns3::Ipv4Address src_ip;
   //The following code is based from
@@ -64,58 +69,67 @@ GetPacketSource(Ptr<Packet> packet)
   return src_ip;
 }
 
-/*************************************************************************************
- * ************************************************************************************
- * ************************************************************************************
- * ************************************************************************************
- */
-ns3::TypeId Agent::GetTypeId(void) {
-    static ns3::TypeId tid = ns3::TypeId("sm4ns3::Agent")
-            .SetParent<ns3::Object > ()
-            .AddConstructor<Agent > ()
-            ;
-    return tid;
+} //End un-named namespace
+
+
+
+/////////////////////////////////////////////////////
+//Agent implementation
+/////////////////////////////////////////////////////
+
+
+sm4ns3::Agent::Agent(int m_AgentId_, BrokerBase* broker) : broker(broker), m_AgentId(m_AgentId_)
+{
+	m_isa = NULL;
 }
 
-std::map<unsigned int,ns3::Ptr<Agent> >& Agent::getAgents()
+sm4ns3::Agent::Agent()
+{
+	m_isa = NULL;
+}
+
+sm4ns3::Agent::~Agent() 
+{
+	if(m_isa) {
+		delete m_isa;
+		m_isa = NULL;
+	}
+}
+
+
+
+ns3::TypeId sm4ns3::Agent::GetTypeId(void) 
+{
+	static ns3::TypeId tid = ns3::TypeId("sm4ns3::Agent")
+		.SetParent<ns3::Object > ()
+		.AddConstructor<Agent > ()
+	;
+	return tid;
+}
+
+
+std::map<unsigned int,ns3::Ptr<Agent> >& sm4ns3::Agent::getAgents()
 {
 	return Agent::AllAgents;
 }
 
-ns3::Ptr<Agent>& Agent::getAgent(unsigned int id){
-	if(Agent::AllAgents.find(id) == Agent::AllAgents.end())
-	{
+ns3::Ptr<Agent>& sm4ns3::Agent::getAgent(unsigned int id)
+{
+	if(Agent::AllAgents.find(id) == Agent::AllAgents.end()) {
 		NS_LOG_UNCOND( "Agent::getAgent Agent not found[" << id  << "]" );
 	}
 	return Agent::AllAgents[id];
 }
 
-Agent::Agent(int m_AgentId_, BrokerBase* broker):m_AgentId(m_AgentId_), broker(broker) {
-//	NS_LOG_UNCOND("Inside Agent::Agent(2)");
-	m_isa = 0;
-//	Agent();
-}
-Agent::Agent(AgentConfig config):m_AgentId(config.id), broker(config.broker) {
-//	NS_LOG_UNCOND("Inside Agent::Agent(1)");
-	m_isa = 0;
-//	Agent();
-}
 
-Agent::Agent(){
-//	NS_LOG_UNCOND("Inside Agent::Agent(0)" << this);
-	m_isa = 0;
-}
-
-
-void Agent::DevRxTrace(std::string context, Ptr<const Packet> p) {
-	std::cout << "Inside Agent::DevRxTrace" << " " << m_AgentId << std::endl;
-//    if (!m_devRxTrace.IsNull())
-//        m_devRxTrace(Ptr<Vehicle > (this), context, p);
-}
-
-void Agent::init()
+void sm4ns3::Agent::DevRxTrace(std::string context, Ptr<const Packet> p) 
 {
-	m_isa = 0;
+	std::cout << "Inside Agent::DevRxTrace" << " " << m_AgentId << std::endl;
+}
+
+void sm4ns3::Agent::init()
+{
+	m_isa = NULL;
 	nc.Create(1);
 	ndc = m_wifi.Install(nc);
 	m_mobility.Install(nc);
@@ -127,37 +141,26 @@ void Agent::init()
 	Bind();
 }
 
-/*ns3::Ptr<Agent> Agent::clone(int m_AgentId_ , sm4ns3::Broker* broker_){
-	NS_LOG_UNCOND("Inside Agent::clone" << this);
-	return ns3::CreateObject<Agent>(m_AgentId_,broker_);
-}*/
-//ns3::Ptr<Agent> Agent::clone(int m_AgentId_ id, sm4ns3::Broker* broker_){
-//	return ns3::CreateObject<Agent>(id,broker_);
-//}
 
-
-/*sm4ns3::BaseFactory<Agent*> &Agent::getFactory(){
-	return m_agentFactory;
-}*/
-
-bool Agent::configure(){
+bool sm4ns3::Agent::configure()
+{
 	return true;
 }
 
-int Agent::Bind(uint16_t port)
+int sm4ns3::Agent::Bind(uint16_t port)
 {
 	if(m_isa) {
 		delete m_isa;
 		m_isa = NULL;
 	}
-    m_isa = new ns3::InetSocketAddress (/*ns3::Ipv4Address::GetAny ()*/GetAddress(), port);
-    if(!m_socket) {
-    	throw std::runtime_error("Can't bind socket");
-    }
-    return m_socket->Bind(*m_isa);
+	m_isa = new ns3::InetSocketAddress (GetAddress(), port);
+	if(!m_socket) {
+		throw std::runtime_error("Can't bind socket");
+	}
+	return m_socket->Bind(*m_isa);
 }
 
-void Agent::ReceivePacket (ns3::Ptr<ns3::Socket> socket)
+void sm4ns3::Agent::ReceivePacket (ns3::Ptr<ns3::Socket> socket)
 {
 	ns3::Ptr<ns3::Packet> packet;
 	std::string str;
@@ -182,23 +185,12 @@ void Agent::ReceivePacket (ns3::Ptr<ns3::Socket> socket)
 }
 
 
-/*
- * Empty destructor
- */
-Agent::~Agent() {
-	if(m_isa)
-	{
-		delete m_isa;
-		m_isa = 0;
-	}
-}
-
-void Agent::AddAgent(unsigned int id, ns3::Ptr<Agent> value)
+void sm4ns3::Agent::AddAgent(unsigned int id, ns3::Ptr<Agent> value)
 {
 	AllAgents.insert(std::make_pair(id, value));
 }
 
-void Agent::RemoveAgent(unsigned int id)
+void sm4ns3::Agent::RemoveAgent(unsigned int id)
 {
 	std::map<unsigned int, ns3::Ptr<Agent> >::iterator it;
 	if((it = AllAgents.find(id)) != AllAgents.end()) {
@@ -207,56 +199,45 @@ void Agent::RemoveAgent(unsigned int id)
 	}
 }
 
-///*
-// * Sets up Wifi for this vehicle using the given helpers
-// */
-//void Agent::SetupWifi(const ns3::WifiHelper &wifi, const ns3::YansWifiPhyHelper &phy, const ns3::NqosWifiMacHelper &mac) {
-//
-//}
-
-/**
- * Returns the unique agent ID for this agent
- */
-int Agent::GetAgentId() {
-    return m_AgentId;
+int sm4ns3::Agent::GetAgentId() 
+{
+	return m_AgentId;
 }
 
-/**
- * Sets the agent ID (should be Unique, use IDGenerator)
- */
-void Agent::SetAgentId(int value) {
+void sm4ns3::Agent::SetAgentId(int value) 
+{
 	m_AgentId = value;
 }
 
-ns3::Ptr<ns3::Node> Agent::GetNode() {
+ns3::Ptr<ns3::Node> sm4ns3::Agent::GetNode() 
+{
 	return m_node;
 }
 
-/**
- * Returns a ns3::Vector representing the location of the center of the
- * back bumper of this vehicle
- */
-ns3::Vector Agent::GetPosition() {
-    return m_node->GetObject<ns3::MobilityModel > ()->GetPosition();
+
+ns3::Vector sm4ns3::Agent::GetPosition() 
+{
+	return m_node->GetObject<ns3::MobilityModel > ()->GetPosition();
 }
 
-/**
- * Sets the position of this vehicle
- */
-void Agent::SetPosition(ns3::Vector value) {
+
+void sm4ns3::Agent::SetPosition(ns3::Vector value) 
+{
 	ns3::Ptr<ns3::MobilityModel> m = m_node->GetObject<ns3::MobilityModel > ();
-    m->SetPosition(value);
+	m->SetPosition(value);
 }
 
-ns3::Ipv4Address Agent::GetAddress() {
-    return iic.GetAddress(0);
+ns3::Ipv4Address sm4ns3::Agent::GetAddress() 
+{
+	return iic.GetAddress(0);
 }
 
-ns3::Address Agent::GetBroadcastAddress() {
-    return m_device->GetBroadcast();
+ns3::Address sm4ns3::Agent::GetBroadcastAddress() 
+{
+	return m_device->GetBroadcast();
 }
 
-int Agent::SendTo(ns3::Ptr<Agent> agent, ns3::Ptr<ns3::Packet> packet) 
+int sm4ns3::Agent::SendTo(ns3::Ptr<Agent> agent, ns3::Ptr<ns3::Packet> packet) 
 {
 	if(!agent) {
 		std::cout <<"Destination Agent is Invalid\n";
@@ -269,148 +250,138 @@ int Agent::SendTo(ns3::Ptr<Agent> agent, ns3::Ptr<ns3::Packet> packet)
 	sm4ns3::Broker::global_pckt_cnt++;
 
 	int res = m_socket->Send(packet);
-   return res;
+	return res;
 }
 
-/*************************************************************************************
- * ************************************************************************************
- * ************************************************************************************
- * ************************************************************************************
- */
+
+/////////////////////////////////////////////////////
+//WFD_Agent implementation
+/////////////////////////////////////////////////////
+
+
+sm4ns3::WFD_Agent::WFD_Agent(int m_AgentId_, BrokerBase* broker):Agent(m_AgentId_,broker)
+{
+}
+
+sm4ns3::WFD_Agent::WFD_Agent():Agent()
+{
+}
+
+sm4ns3::WFD_Agent::~WFD_Agent() 
+{
+	if(m_isa) {
+		delete m_isa;
+		m_isa = NULL;
+	}
+}
+
 
 //and apply this to the base class
-ns3::NodeContainer WFD_Agent::wfd_nc;
-ns3::NetDeviceContainer WFD_Agent::wfd_ndc ;
-std::map<unsigned int , ns3::NodeContainer>  WFD_Agent::wfd_go_nc;
-std::map<unsigned int , ns3::NetDeviceContainer>  WFD_Agent::wfd_go_ndc ;
-std::map<unsigned int , ns3::NodeContainer>  WFD_Agent::wfd_cln_nc;
-std::map<unsigned int , ns3::NetDeviceContainer>  WFD_Agent::wfd_cln_ndc ;
-WifiHelper WFD_Agent::wfd_wifi;
-NqosWifiMacHelper WFD_Agent::wfd_mac = NqosWifiMacHelper::Default ();
-YansWifiPhyHelper WFD_Agent::wfd_wifiPhy = YansWifiPhyHelper::Default ();
-YansWifiChannelHelper WFD_Agent::wfd_wifiChannel = YansWifiChannelHelper::Default ();
-ns3::InternetStackHelper WFD_Agent::wfd_internet;
-ns3::MobilityHelper WFD_Agent::wfd_mobility;
-ns3::Ptr<ListPositionAllocator> WFD_Agent::wfd_positionAlloc;
-ns3::Ipv4AddressHelper WFD_Agent::wfd_ipAddrs;
+ns3::NodeContainer sm4ns3::WFD_Agent::wfd_nc;
+ns3::NetDeviceContainer sm4ns3::WFD_Agent::wfd_ndc ;
+std::map<unsigned int , ns3::NodeContainer>  sm4ns3::WFD_Agent::wfd_go_nc;
+std::map<unsigned int , ns3::NetDeviceContainer>  sm4ns3::WFD_Agent::wfd_go_ndc ;
+std::map<unsigned int , ns3::NodeContainer>  sm4ns3::WFD_Agent::wfd_cln_nc;
+std::map<unsigned int , ns3::NetDeviceContainer>  sm4ns3::WFD_Agent::wfd_cln_ndc ;
+WifiHelper sm4ns3::WFD_Agent::wfd_wifi;
+ns3::NqosWifiMacHelper WFD_Agent::wfd_mac = ns3::NqosWifiMacHelper::Default ();
+ns3::YansWifiPhyHelper WFD_Agent::wfd_wifiPhy = ns3::YansWifiPhyHelper::Default ();
+ns3::YansWifiChannelHelper WFD_Agent::wfd_wifiChannel = ns3::YansWifiChannelHelper::Default ();
+ns3::InternetStackHelper sm4ns3::WFD_Agent::wfd_internet;
+ns3::MobilityHelper sm4ns3::WFD_Agent::wfd_mobility;
+ns3::Ptr<ListPositionAllocator> sm4ns3::WFD_Agent::wfd_positionAlloc;
+ns3::Ipv4AddressHelper sm4ns3::WFD_Agent::wfd_ipAddrs;
 
-WFD_Agent::AgentRole WFD_Agent::getRole(){
+
+WFD_Agent::AgentRole sm4ns3::WFD_Agent::getRole()
+{
 	return role;
 }
 
-void WFD_Agent::setRole(AgentRole role_){
+void sm4ns3::WFD_Agent::setRole(AgentRole role_)
+{
 	role = role_;
 }
 
-WFD_Agent::WFD_Agent(int m_AgentId_, BrokerBase* broker):Agent(m_AgentId_,broker){
-	NS_LOG_UNCOND("Inside WFD_Agent::WFD_Agent(2)");
-}
-WFD_Agent::WFD_Agent():Agent(){
-	NS_LOG_UNCOND("Inside WFD_Agent::WFD_Agent(0)" << this);
-}
-//per agent initialization
-void WFD_Agent::init(){
 
+void sm4ns3::WFD_Agent::init()
+{
 	NS_LOG_UNCOND("Inside WFD_Agent::init");
 	m_wfd_node = ns3::CreateObject<ns3::Node> ();
-    //
-    // fill in proper container to manage the nodes of the LAN.  We need
-    // at least three containers here; one with all of the client nodes,
+
+	// fill in proper container to manage the nodes of the LAN.  We need
+	// at least three containers here; one with all of the client nodes,
 	// one with GO and one with all of the nodes including go and client nodes
 	//todo, this function and doRoleAssignment in WFD_registration are doing the same thing. must cancel one and use the results of the other one
 	figureOutGroup();//trivial function to ,later , support multi roups
 
-	if(getRole() == WFD_GO){
+	if(getRole() == WFD_GO) {
 		wfd_go_nc[groupId].Add(m_wfd_node);
-	}
-	else if(getRole() == WFD_CLIENT){
+	} else if(getRole() == WFD_CLIENT) {
 		wfd_cln_nc[groupId].Add(m_wfd_node);
 	}
 
 	wfd_nc.Add(m_wfd_node);
-
 }
-/*ns3::Ptr<Agent> WFD_Agent::clone(int m_AgentId_ , sm4ns3::Broker* broker_){
 
-//	NS_LOG_UNCOND("Inside WFD_Agent::clone" << this);
-	return ns3::CreateObject<WFD_Agent>(m_AgentId_,broker_);
-}*/
 
-void WFD_Agent::figureOutGroup(){
+void sm4ns3::WFD_Agent::figureOutGroup()
+{
 	groupId = 0;//for now
 	setRole(((this->GetAgentId() == AllAgents.begin()->second->GetAgentId()) ? WFD_GO : WFD_CLIENT));
 }
 
-//all agents configuration
-void WFD_Agent::configAll(){
-	  // Reset the address base--the networks will be in
-	  // the "10.0" address space
+void sm4ns3::WFD_Agent::configAll()
+{
+	// Reset the address base--the networks will be in the "10.0" address space
 	wfd_ipAddrs.SetBase ("10.0.0.0", "255.255.255.0");
-	  for (uint32_t groupId_ = 0; groupId_ < wfd_go_nc.size() ; ++groupId_)
-	    {
-	      //
-	      // Create an infrastructure network
-	      //
-	      wfd_wifi = WifiHelper::Default ();
-	      wfd_mac = NqosWifiMacHelper::Default ();
-	      wfd_wifiPhy.SetChannel (wfd_wifiChannel.Create ());
-	      // Create unique ssids for these networks
-	      std::string ssidString ("WFD-GRP-");
-	      std::stringstream ss;
-	      ss << groupId_;
-	      ssidString += ss.str ();
-	      ns3::Ssid ssid = Ssid (ssidString);
-	      wfd_wifi.SetRemoteStationManager ("ns3::ArfWifiManager");
-	      // setup stas
-	      wfd_mac.SetType ("ns3::StaWifiMac",
-	                        "Ssid", SsidValue (ssid),
-	                        "ActiveProbing", BooleanValue (false));
-	      wfd_cln_ndc[groupId_] = wfd_wifi.Install (wfd_wifiPhy, wfd_mac, wfd_cln_nc[groupId_]);
-	      // setup ap.
-	      wfd_mac.SetType ("ns3::ApWifiMac",
-	                        "Ssid", SsidValue (ssid));
-	      wfd_go_ndc[groupId_] = wfd_wifi.Install (wfd_wifiPhy, wfd_mac, wfd_go_nc[groupId_].Get(0));
-	      // Collect all of these new devices and nodes
-	      NetDeviceContainer grpDevices (wfd_go_ndc[groupId_], wfd_cln_ndc[groupId_]);
-	      NodeContainer grpNodes (wfd_go_nc[groupId_], wfd_cln_nc[groupId_]);
-	      //d
-	      // Add the IPv4 protocol stack to the nodes in our container
-	      //
-	      NS_LOG_UNCOND("installing internet stack for " << grpNodes.GetN() << " nodes\n");
-//	      InternetStackHelper internet;
-//	      internet.Install(grpNodes);
+	for (uint32_t groupId_ = 0; groupId_ < wfd_go_nc.size() ; ++groupId_) {
+		//Create an infrastructure network
+		wfd_wifi = WifiHelper::Default ();
+		wfd_mac = NqosWifiMacHelper::Default ();
+		wfd_wifiPhy.SetChannel (wfd_wifiChannel.Create ());
 
-	      wfd_internet.Install (grpNodes);
-	      NS_LOG_UNCOND("installing internet stack for " << grpNodes.GetN() << " nodes done\n");
-	      //
-	      // Assign IPv4 addresses to the device drivers (actually to the associated
-	      // IPv4 interfaces) we just created.
-	      //
-	      Ipv4InterfaceContainer wfd_addresses = wfd_ipAddrs.Assign (grpDevices);
-	      Ipv4InterfaceContainer::Iterator it;
-	      for(it = wfd_addresses.Begin(); it != wfd_addresses.End(); it++){
-	    	  NS_LOG_UNCOND(it->first->GetAddress(1,0));
-	      }
-	      //
-	      // Assign a new network prefix for each mobile network, according to
-	      // the network mask initialized above
-	      //
-	      wfd_ipAddrs.NewNetwork ();
-//	      //
-//	      // The new wireless nodes need a mobility model so we aggregate one
-//	      // to each of the nodes we just finished building.
-//	      //
-//	      Ptr<ListPositionAllocator> subnetAlloc =
-//	        CreateObject<ListPositionAllocator> ();
-//	      for (uint32_t j = 0; j < grpNodes.GetN (); ++j)
-//	        {
-//	          subnetAlloc->Add (Vector (0.0, j, 0.0));
-//	        }
-	      wfd_mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-	      wfd_mobility.Install (grpNodes);
-	    }
+		// Create unique ssids for these networks
+		std::string ssidString ("WFD-GRP-");
+		
+		std::stringstream ss;
+		ss << groupId_;
+		ssidString += ss.str ();
 
+		ns3::Ssid ssid = Ssid (ssidString);
+		wfd_wifi.SetRemoteStationManager ("ns3::ArfWifiManager");
+
+		// setup stas
+		wfd_mac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (ssid), "ActiveProbing", BooleanValue (false));
+		wfd_cln_ndc[groupId_] = wfd_wifi.Install (wfd_wifiPhy, wfd_mac, wfd_cln_nc[groupId_]);
+
+		// setup ap.
+		wfd_mac.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (ssid));
+		wfd_go_ndc[groupId_] = wfd_wifi.Install (wfd_wifiPhy, wfd_mac, wfd_go_nc[groupId_].Get(0));
+
+		// Collect all of these new devices and nodes
+		NetDeviceContainer grpDevices (wfd_go_ndc[groupId_], wfd_cln_ndc[groupId_]);
+		NodeContainer grpNodes (wfd_go_nc[groupId_], wfd_cln_nc[groupId_]);
+
+		// Add the IPv4 protocol stack to the nodes in our container
+		NS_LOG_UNCOND("installing internet stack for " << grpNodes.GetN() << " nodes\n");
+		wfd_internet.Install (grpNodes);
+		NS_LOG_UNCOND("installing internet stack for " << grpNodes.GetN() << " nodes done\n");
+
+		// Assign IPv4 addresses to the device drivers (actually to the associated
+		// IPv4 interfaces) we just created.
+		Ipv4InterfaceContainer wfd_addresses = wfd_ipAddrs.Assign (grpDevices);
+		Ipv4InterfaceContainer::Iterator it;
+		for(it = wfd_addresses.Begin(); it != wfd_addresses.End(); it++) {
+			NS_LOG_UNCOND(it->first->GetAddress(1,0));
+		}
+
+		// Assign a new network prefix for each mobile network, according to
+		// the network mask initialized above
+		wfd_ipAddrs.NewNetwork ();
+		wfd_mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+		wfd_mobility.Install (grpNodes);
+	}
 }
-} /* namespace sm4ns3 */
 
 
--- model/smb_agent.h
+++ model/smb_agent.h
@@ -22,144 +22,124 @@ class Ipv4InterfaceContainer;
 class WifiHelper;
 class YansWifiPhyHelper;
 class NqosWifiMacHelper;
-}
+} //End namespace ns3
 
-using namespace ns3;
+
 namespace sm4ns3 {
+
 class BrokerBase;
-//NS_LOG_COMPONENT_DEFINE ("A NAME_sadjcbsajkl");
-struct AgentConfig {
-	unsigned int id;
-	unsigned int type;
-	BrokerBase * broker;
-	AgentConfig(unsigned int id,unsigned int type, BrokerBase*broker):
-		id(id),
-		type(type),
-		broker(broker){}
-};
-class Agent : public ns3::Object{
+
+
+class Agent : public ns3::Object {
 public:
-	ns3::Ptr<ns3::Node> m_node; // Agent has a node
-    int m_AgentId; // Agent's id
-    sm4ns3::BrokerBase* broker;
-    ns3::Ptr<ns3::NetDevice> m_device; // Agent has a device
-
-//    sm4ns3::BaseWifi m_basic_wifi;
-    ns3::NodeContainer nc;
-    ns3::NetDeviceContainer ndc ;
-    ns3::Ipv4InterfaceContainer iic;
-
-    static BaseWifi m_wifi;
-    static BaseMobility m_mobility;
-	static BaseIP m_ip;
-
-	ns3::Ptr<ns3::Socket> m_socket;
-	ns3::InetSocketAddress* m_isa;
-
-
-	//All agents, sorted by agent ID
-	static std::map<unsigned int,ns3::Ptr<Agent> > AllAgents;
-
-    /// agent factory(the best place to keep this factory is here itself(or may be a singleton)
-//    static sm4ns3::BaseFactory<Agent*> m_agentFactory;
-public:
-    /// Override TypeId.
-    static ns3::TypeId GetTypeId(void);
-    //	constructor
+	//	constructor
 	Agent(int m_AgentId_, BrokerBase* broker);
-	Agent(AgentConfig config);
 	Agent();
-	void DevRxTrace(std::string context, Ptr<const Packet> p);
+	virtual ~Agent();
+
+	/// Override TypeId.
+	static ns3::TypeId GetTypeId(void);
+
+	void DevRxTrace(std::string context, ns3::Ptr<const ns3::Packet> p);
 	virtual void init();
 
 	static void SetBasicNetworking(
-			const ns3::WifiHelper &wifi = ns3::WifiHelper::Default(),
-			const ns3::YansWifiPhyHelper &phy = ns3::YansWifiPhyHelper::Default(),
-			const ns3::NqosWifiMacHelper &mac = ns3::NqosWifiMacHelper::Default());
-//	void Configure(sm4ns3::BaseWifi &baseWifi,
-//			sm4ns3::BaseMobility &baseMobility,
-//			sm4ns3::BaseIP &baseIP = m_ip);
+		const ns3::WifiHelper &wifi = ns3::WifiHelper::Default(),
+		const ns3::YansWifiPhyHelper &phy = ns3::YansWifiPhyHelper::Default(),
+		const ns3::NqosWifiMacHelper &mac = ns3::NqosWifiMacHelper::Default()
+	);
 
-//	void Configure(sm4ns3::APP_TYPE);
-////	//returns agent's InetSocketAddress
-////	ns3::InetSocketAddress &getISA();
-//	ns3::Ptr<ns3::Socket> & Socket();
 	virtual bool configure();
 	int Bind(uint16_t port = 80);
 	void ReceivePacket (ns3::Ptr<ns3::Socket> socket);
-//	//	destructor
-	virtual ~Agent();
-    static std::map<unsigned int,ns3::Ptr<Agent> > &getAgents();
-    static ns3::Ptr<Agent>& getAgent(unsigned int);
+
+	static std::map<unsigned int,ns3::Ptr<Agent> > &getAgents();
+	static ns3::Ptr<Agent>& getAgent(unsigned int);
+
 	//	add agents
 	static void AddAgent(unsigned int , ns3::Ptr<Agent>);
 	static void RemoveAgent(unsigned int);
 
-//	/**
-//     * \param wifi a WifiHelper.
-//     * \param phy  a YansWifiPhyHelper.
-//     * \param mac  a NqosWifiMacHelper.
-//     *
-//     * Setups the Agent wifi given the appropriate helpers.
-//     * This function may need to be modified later if stable WAVE/DSRC standards added to ns-3.
-//     */
-//	virtual void SetupWifi(const ns3::WifiHelper &wifi, const ns3::YansWifiPhyHelper &phy, const ns3::NqosWifiMacHelper &mac);
+	///returns the Agent Id.
+	int GetAgentId();
 
-    /**
-     * \returns the Agent Id.
-     *
-     */
-    int GetAgentId();
-    /**
-     * \param value a Agent Id.
-     *
-     * A Agent can have an Id. It is good that this Id be unique in the VANETs Highway.
-     */
-    void SetAgentId(int value);
-    /**
-     * \returns the position of Agent's Node which is loacted at the center back of the Agent.
-     */
-    ns3::Ptr<ns3::Node> GetNode();
-    ns3::Vector GetPosition();
-    /**
-     * \param value a position Vector.
-     *
-     * This function sets the position of Agent's Node. Agent's position is its Node's position.
-     * This position Vector must point to the center back of the Agent.
-     */
-    void SetPosition(ns3::Vector value);
-    /**
-     * \return the Wifi address of the Agent.
-     *
-     * Agents can use this address to communicate to each other in an unicast fashion.
-     */
-    ns3::Ipv4Address GetAddress();
-    /**
-     * \returns the Wifi broadcast address.
-     *
-     * A Agent can broadcast messages using this address.
-     */
-    ns3::Address GetBroadcastAddress();
+	///Agent can have an Id. It is good that this Id be unique in the VANETs Highway.
+	void SetAgentId(int value);
 
-//    sm4ns3::BaseWifi & getBaseWifi();
-    /**
-     * \param agent the destination agent.
-     * \param packet the packet to send.
-     * \returns whatever the socket->Send returns
-     */
-    int SendTo(ns3::Ptr<Agent> agent, ns3::Ptr<ns3::Packet> packet);
+	///\returns the position of Agent's Node which is loacted at the center back of the Agent.
+	ns3::Ptr<ns3::Node> GetNode();
+
+	ns3::Vector GetPosition();
+
+	///This function sets the position of Agent's Node. Agent's position is its Node's position.
+	///This position Vector must point to the center back of the Agent.
+	void SetPosition(ns3::Vector value);
+
+	///Agents can use this address to communicate to each other in an unicast fashion.
+	///\return the Wifi address of the Agent.
+	ns3::Ipv4Address GetAddress();
+
+	///A Agent can broadcast messages using this address.
+	///\returns the Wifi broadcast address.
+	ns3::Address GetBroadcastAddress();
+
+
+	///\param agent the destination agent.
+	///\param packet the packet to send.
+	///\returns whatever the socket->Send returns
+	int SendTo(ns3::Ptr<Agent> agent, ns3::Ptr<ns3::Packet> packet);
+
+public:
+	//All agents, sorted by agent ID
+	static std::map<unsigned int,ns3::Ptr<Agent> > AllAgents;
+
+protected:
+	//Interface to the Broker
+	sm4ns3::BrokerBase* broker;
+
+	int m_AgentId; // Agent's id
+	ns3::Ptr<ns3::Node> m_node; // Agent has a node
+	ns3::Ptr<ns3::NetDevice> m_device; // Agent has a device
+
+	ns3::NodeContainer nc;
+	ns3::NetDeviceContainer ndc ;
+	ns3::Ipv4InterfaceContainer iic;
+
+	static BaseWifi m_wifi;
+	static BaseMobility m_mobility;
+	static BaseIP m_ip;
+
+	ns3::Ptr<ns3::Socket> m_socket;
+	ns3::InetSocketAddress* m_isa;
 };
 
-/*
- * ******************************************************
- */
+
+
 
 class WFD_Agent :public Agent{
 public:
+	WFD_Agent(int m_AgentId_, BrokerBase* broker);
+	WFD_Agent();
+	virtual ~WFD_Agent();
+
 	enum AgentRole{
 		WFD_GO,
 		WFD_CLIENT
 	};
+
+public:
+	void init();
+	AgentRole getRole();
+	void setRole(AgentRole);
+
+	//which group are you in and which role do you have?
+	void figureOutGroup();
+
+	///\param agent the destination agent.
+	///\param packet the packet to send.
+	///\returns whatever the socket->Send returns
+	static void configAll();
+
 private:
 	ns3::Ptr<ns3::Node> m_wfd_node; // Agent has a node
 	AgentRole role;
@@ -170,37 +150,22 @@ private:
 	//if the results are good, remove wfd prefix
 
 	//and apply this to the base class
-    static ns3::NodeContainer wfd_nc;
-    static ns3::NetDeviceContainer wfd_ndc ;
-    static std::map<unsigned int , ns3::NodeContainer> wfd_go_nc;
-    static std::map<unsigned int , ns3::NetDeviceContainer> wfd_go_ndc ;
-    static std::map<unsigned int , ns3::NodeContainer> wfd_cln_nc;
-    static std::map<unsigned int , ns3::NetDeviceContainer> wfd_cln_ndc ;
-    static ns3::WifiHelper wfd_wifi;
-    static ns3::NqosWifiMacHelper wfd_mac;
-    static ns3::YansWifiPhyHelper wfd_wifiPhy;
-    static ns3::YansWifiChannelHelper wfd_wifiChannel;
-    static InternetStackHelper wfd_internet;
-    static ns3::MobilityHelper wfd_mobility;
-    static ns3::Ptr<ListPositionAllocator> wfd_positionAlloc;
-    static ns3::Ipv4AddressHelper wfd_ipAddrs;
-public:
-
-	WFD_Agent(int m_AgentId_, BrokerBase* broker);
-	WFD_Agent();
-	void init();
-	AgentRole getRole();
-	void setRole(AgentRole);
-//	ns3::Ptr<Agent> clone(int m_AgentId_, sm4ns3::Broker* broker_);
-
-	//which group are you in and which role do you have?
-	void figureOutGroup();
-	    /**
-	     * \param agent the destination agent.
-	     * \param packet the packet to send.
-	     * \returns whatever the socket->Send returns
-	     */
-	static void configAll();
+	static ns3::NodeContainer wfd_nc;
+	static ns3::NetDeviceContainer wfd_ndc ;
+	static std::map<unsigned int , ns3::NodeContainer> wfd_go_nc;
+	static std::map<unsigned int , ns3::NetDeviceContainer> wfd_go_ndc ;
+	static std::map<unsigned int , ns3::NodeContainer> wfd_cln_nc;
+	static std::map<unsigned int , ns3::NetDeviceContainer> wfd_cln_ndc ;
+	static ns3::WifiHelper wfd_wifi;
+	static ns3::NqosWifiMacHelper wfd_mac;
+	static ns3::YansWifiPhyHelper wfd_wifiPhy;
+	static ns3::YansWifiChannelHelper wfd_wifiChannel;
+	static ns3::InternetStackHelper wfd_internet;
+	static ns3::MobilityHelper wfd_mobility;
+	static ns3::Ptr<ns3::ListPositionAllocator> wfd_positionAlloc;
+	static ns3::Ipv4AddressHelper wfd_ipAddrs;
 };
 
-} /* namespace sm4ns3 */
+}
+
+
